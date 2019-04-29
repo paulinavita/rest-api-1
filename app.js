09885a082312ab157f237a13c26ba086a25d3d36
paulinavita <paulinadavita@gmail.com>
2019-04-29 16:47:57 +0700

    revisi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log(process.env.JWT_SECRET)
 const express = require("express")
 const app = express()
 const routes = require("./routes")
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(express.urlencoded({extends: true}))
